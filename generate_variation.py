1fc4b47a575ee64a796e371390bb127546875cf0
Ashok Poudel <ashok.poudel@gmail.com>
2023-03-22 11:36:45 -0600

    First Commit

---
+++ generate_variation.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+from lib.color_replacer import ColorReplacer
+from lib.openai_color_fetcher import OpenAIColorFetcher
+from dotenv import load_dotenv
+import os
+import re
+import json
+import argparse
+
+
+# Function to parse colors from the response
+def parse_colors_from_response(color_response):
+   # Replace single quotes with double quotes to make the string JSON-compatible
+    color_string = color_response.replace("'", "\"")
+
+    # Load the JSON string into a dictionary
+    colors = json.loads(color_string)
+
+    print(f"Parsed colors: {colors}")  # Add this print statement
+
+    return colors
+
+def main(style_for):
+    input_file = './data/basetemplate.html'
+    output_file = f'./data/{style_for.replace(" ", "_")}.html'
+
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    color_fetcher = OpenAIColorFetcher(api_key)
+    color_response = color_fetcher.get_prompt(style_for)
+    print("---- Style Response --------")
+    print(color_response)
+    colors = parse_colors_from_response(color_response)
+
+    with open(input_file, 'r') as file:
+        html = file.read()
+
+    color_replacer = ColorReplacer(html)
+    color_replacer.replace_colors(colors)
+    color_replacer.replace_typography(colors['font'], colors['font_style'])
+    updated_html = color_replacer.get_updated_html()
+
+    with open(output_file, 'w') as file:
+        file.write(updated_html)
+
+    print(f'Updated HTML saved to {output_file}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--style_for', type=str, required=True, help='Who is this style for? for example: a traditional Mexican Resturant')
+    args = parser.parse_args()
+    main(args.style_for)
---
+++ readme.md
@@ -0,0 +1,36 @@
+Generate Grape.js Template Variations using GPT-4
+==================
+
+This script generates variations of a grape.js newsletter template HTML file by using OpenAI's GPT-4 API to fetch color and typography suggestions based on a provided style.
+
+Installation
+------------
+
+1.  Clone this repository.
+2.  Install the required packages using `pip install -r requirements.txt`.
+3.  Rename `.env.example` to `.env` and replace the placeholder API key with your own.
+
+Usage
+-----
+
+To use the script, run the following command:
+
+pythonCopy code
+
+`python generate_variation.py --style_for "my restaurant style"`
+
+This will generate a new HTML file with the specified style applied. The output file will be saved to the `./data` directory with a filename based on the provided `--style_for` argument.
+
+For example, if you run `python generate_variation.py --style_for "modern fusion restaurant"`, the output file will be saved to `./data/modern_fusion_restaurant.html`.
+
+Data
+----
+
+This script uses a base HTML template file located in the `./data` directory. You can modify this file to change the structure and content of the output HTML files.
+
+The script also uses OpenAI's GPT-3 API to fetch color and typography suggestions. You will need to provide your own API key in the `.env` file.
+
+License
+-------
+
+This project is licensed under the MIT License. Feel free to modify and use it however you like!
